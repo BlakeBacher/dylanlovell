bbe5b1b63eb981ad6731ba306d7f51eb0ae1af64
Dylan <36043075+Dylovell@users.noreply.github.com>
2018-06-29 15:42:39 -0600

    routing really works

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require('express')
-var path = require('path');
 const app = express();
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dylan.html'));
-});
+app.use(express.static(__dirname+'/public'))
 
 app.listen(3050)
