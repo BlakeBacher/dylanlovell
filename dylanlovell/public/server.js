375e14eac3deef76873e52ef79d90598aa278e54
Dylan Lovell <Dylovell@live.com>
2018-07-30 15:04:54 -0600

    hey blake

--- dylanlovell/public/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-var path = require('path');
-const app = express();
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/'));
-});
-
-app.listen(3050)
